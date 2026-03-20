0bd891bc36dc8cfe3a498fb620bd694183ecc913
alihassan200721 <alihassanalihasssan200721@gmail.com>
2026-03-20 10:36:56 +0500

    Update

--- bank.py
+++ bank.py
@@ -1,1 +1,10 @@
-gretting = input()
+gretting = input("Grettings: ").lower()
+
+if gretting == "hello":
+    print("$0")
+
+elif gretting.startswith("h"):
+    print("$20")
+
+else:
+    print("$100")
