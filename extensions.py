201c5a331085d78fc6c8cc0d87320c354fceead1
alihassan200721 <alihassanalihasssan200721@gmail.com>
2026-03-22 11:02:56 +0500

    Update

--- extensions.py
+++ extensions.py
@@ -1,2 +1,18 @@
-user_input = input("File name: ").lower()
+name = input("File name: ").strip().lower()
 
+media_types = {
+    ".gif": "image/gif",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".pdf": "application/pdf",
+    ".txt": "text/plain",
+    ".zip": "application/zip",
+}
+
+for ext, media_type in media_types.items():
+    if name.endswith(ext):
+        print(media_type)
+        break
+else:
+    print("application/octet-stream")
